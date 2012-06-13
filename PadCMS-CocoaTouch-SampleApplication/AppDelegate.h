ac88df3b480463f10a653ae2508e839fc271d081
Oleg V. Zhitnik <blacklion.ozv@gmail.com>
2012-06-13 15:46:02 +0300

    Copyright fixes

--- PadCMS-CocoaTouch-SampleApplication/AppDelegate.h
+++ PadCMS-CocoaTouch-SampleApplication/AppDelegate.h
@@ -3,7 +3,7 @@
 //  PadCMS-CocoaTouch-SampleApplication
 //
 //  Created by Oleg Zhitnik on 13.06.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Adyax. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
